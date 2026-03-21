9be57c9a12553d5953bccc744d8cf2eb4f56f8f0
OjaswitaKapadne <ojaswitakapadne1010@gmail.com>
2026-03-21 13:18:32 +0530

    Data Validation

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -22,3 +22,15 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float =  0.2
+
+## Updating all the constantss related to data validation
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIRECTORY: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+## Schema file path
+
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
